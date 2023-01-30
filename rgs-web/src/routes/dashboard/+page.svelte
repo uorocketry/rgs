7f335e1d56f13615e13f64dc117fff4a082fc189
Fernando Nogueira <abert036@uottawa.ca>
2023-01-30 04:09:33 -0500

    Add satellite maps and NavBall (#10)

--- rgs-web/src/routes/dashboard/+page.svelte
+++ rgs-web/src/routes/dashboard/+page.svelte
@@ -1,5 +1,36 @@
-<script defer lang="ts" type="module">
+<script lang="ts" type="module">
+  import NavBall from "$lib/components/NavBall.svelte";
+  import type { AbsoluteOrientationSensor } from "motion-sensors-polyfill";
+  import * as THREE from "three";
+  import { Euler } from "three";
+
+  let rotation: THREE.Quaternion = new THREE.Quaternion();
+  // @ts-ignore: Can't import constructor
+  // let sensor: AbsoluteOrientationSensor = new AbsoluteOrientationSensor({
+  //   frequency: 60,
+  // });
+  // sensor.addEventListener("reading", (e: any) => {
+  //   if (e.target?.quaternion) {
+  //     rotation.set(
+  //       e.target.quaternion[0],
+  //       e.target.quaternion[1],
+  //       e.target.quaternion[2],
+  //       e.target.quaternion[3]
+  //     );
+  //   }
+  // });
+  // sensor.start();
+
+  // Every seconds pick a random rotation
+  setInterval(() => {
+    rotation.setFromEuler(
+      new Euler(Math.random() * 7, Math.random() * 7, Math.random() * 7)
+    );
+  }, 1000);
 </script>
 
-<p>Wow! Such empty! We would probably want to see some real time data here.</p>
-<p>Take a look a Logging for a minimalist (and not type-safe) example</p>
+<div class="flex h-full items-center place-content-center">
+  <div class="w-[48rem] h-[48rem]">
+    <NavBall bind:targetRotation="{rotation}" />
+  </div>
+</div>
