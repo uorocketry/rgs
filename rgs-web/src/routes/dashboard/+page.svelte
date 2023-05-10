3f8e2a11536b65b5a0a0ac529b860de20979f783
Noah Sprenger <noah.r.sprenger@gmail.com>
2023-05-10 00:36:05 -0400

    Cleanup dashboard

--- rgs-web/src/routes/dashboard/+page.svelte
+++ rgs-web/src/routes/dashboard/+page.svelte
@@ -6,25 +6,9 @@
 
   let rotation: THREE.Quaternion = new THREE.Quaternion();
 
-  // Every seconds pick a random rotation
-  // setInterval(() => {
-  //   rotation.setFromEuler(
-  //     new Euler(Math.random() * 7, Math.random() * 7, Math.random() * 7)
-  //   );
-  // }, 1000);
-
   onSocket("RocketData", (packet) => {
-    // console.log(packet.RocketData);
     if (packet.RocketData.data.sensor === undefined) return;
     if (packet.RocketData.data.sensor.data.Sbg === undefined) return;
-
-    // console.log(packet.RocketData.data.sensor.data.Sbg);
-    console.log({
-      pitch: packet.RocketData.data.sensor.data.Sbg?.pitch,
-      roll: packet.RocketData.data.sensor.data.Sbg?.roll,
-      yaw: packet.RocketData.data.sensor.data.Sbg?.yaw,
-    });
-    const toDeg = (rad: number) => (rad * 180) / Math.PI;
     rotation.setFromEuler(
       new Euler(
         packet.RocketData.data.sensor.data.Sbg?.quant_x,
