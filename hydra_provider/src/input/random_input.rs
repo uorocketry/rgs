79e5f39c09656b8446af56c17b3cab2d3b6022cb
Jonathan Papineau <dev@jontech.app>
2023-06-08 23:42:14 -0400

    wip

--- hydra_provider/src/input/random_input.rs
+++ hydra_provider/src/input/random_input.rs
@@ -1,8 +1,10 @@
 use crate::processing::InputData;
 use messages::mavlink::MavHeader;
 use messages::sender::Sender;
-use messages::sensor::{Sbg, Sensor};
 use messages::Message;
+use proptest::arbitrary::Arbitrary;
+use proptest::strategy::{Strategy, ValueTree};
+use proptest::test_runner::TestRunner;
 use rand::rngs::ThreadRng;
 use rand::Rng;
 use std::time::Duration;
@@ -29,37 +31,6 @@ impl RandomInput {
         }
     }
 
-    fn random_sbg(&mut self) -> Message {
-        let sbg = Sbg {
-            accel_x: self.rng.gen(),
-            accel_y: self.rng.gen(),
-            accel_z: self.rng.gen(),
-            velocity_n: self.rng.gen(),
-            velocity_e: self.rng.gen(),
-            pressure: self.rng.gen(),
-            height: self.rng.gen(),
-            roll: self.rng.gen(),
-            yaw: self.rng.gen(),
-            pitch: self.rng.gen(),
-            latitude: self.rng.gen(),
-            longitude: self.rng.gen(),
-            quant_w: self.rng.gen(),
-            quant_x: self.rng.gen(),
-            quant_y: self.rng.gen(),
-            velocity_d: self.rng.gen(),
-            quant_z: self.rng.gen(),
-        };
-
-        let time = fugit::Instant::<u64, 1, 1000>::from_ticks(
-            SystemTime::now()
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap()
-                .as_millis() as u64,
-        );
-
-        Message::new(&time, Sender::MainBoard, Sensor::new(0, sbg))
-    }
-
     fn random_mavheader(&mut self) -> MavHeader {
         MavHeader {
             system_id: 0,
@@ -68,11 +39,21 @@ impl RandomInput {
         }
     }
 
+    fn random_message() -> Message {
+        // This takes advantage of the proptest library that we are already using to test the
+        // message.
+        let mut runner = TestRunner::default();
+
+        Message::arbitrary()
+            .new_tree(&mut runner)
+            .unwrap()
+            .current()
+    }
+
     fn read_message(&mut self) -> InputData {
         match self.rng.gen_range(0..=1) {
-            0 => InputData::RocketData(self.random_sbg()),
+            0 => InputData::RocketData(RandomInput::random_message()),
             _ => InputData::MavlinkHeader(self.random_mavheader()),
         }
-
     }
 }
