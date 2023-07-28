5ce910b23c8ac49854081a6b97565ff4c52e4480
Fernando Nogueira <abert036@uottawa.ca>
2023-07-28 06:55:25 -0300

    chore: remove unused on web mock

--- web/io/server.ts
+++ web/io/server.ts
@@ -30,48 +30,5 @@ export const setupServer = (http: HTTPServer) => {
 		socket.on('ping', (cb: Function) => {
 			cb(Date.now());
 		});
-
-		// Mock commands:
-		// mock/start | Start a zmq publisher binding to port
-		// mock/stop  | Stop the zmq publisher
-		// mock/send  | Send a message to the zmq publisher
-		// mock/status | Get the status of the zmq publisher (running or not)
-		socket.on('mock/start', async (port: number) => {
-			if (sockState === 'bound') {
-				socket.emit('mock/status', 'Already bound');
-				return;
-			} else if (sockState === 'binding') {
-				socket.emit('mock/status', 'Already binding');
-				return
-			}
-			socket.emit('mock/status', 'Binding');
-			await zmqSock.bind(`tcp://*:${process.env.ZMQ_PORT ?? '3002'}`);
-			sockState = 'bound';
-			socket.emit('mock/status', 'Bound');
-		});
-
-		socket.on('mock/stop', async () => {
-			if (sockState === 'unbound') {
-				socket.emit('mock/status', 'Already unbound');
-				return;
-			}
-			socket.emit('mock/status', 'Unbinding');
-			await zmqSock.unbind(`tcp://*:${process.env.ZMQ_PORT ?? '3002'}`);
-			sockState = 'unbound';
-		});
-
-		socket.on('mock/send', async (msg: string) => {
-			if (sockState === 'unbound' || sockState === 'binding') {
-				socket.emit('mock/status', 'Not bound');
-				return;
-			}
-			// Act as a proxy for the client
-			await zmqSock.send(msg);
-			socket.emit('mock/status', 'Sent message');
-		});
-
-		socket.on('mock/status', () => {
-			socket.emit('mock/status', sockState);
-		});
 	});
 };
