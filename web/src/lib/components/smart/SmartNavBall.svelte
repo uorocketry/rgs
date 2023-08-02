105c0428d67c08740d5930058fdbc57d2f479d3d
Fernando Nogueira <abert036@uottawa.ca>
2023-08-02 16:49:58 -0300

    foo

--- web/src/lib/components/smart/SmartNavBall.svelte
+++ web/src/lib/components/smart/SmartNavBall.svelte
@@ -1,26 +1,89 @@
 <script lang="ts">
-	import { Euler, Quaternion } from 'three';
-	import NavBall from '../NavBall.svelte';
+	import { Euler, Quaternion, Vector3, type EulerOrder, Matrix4 } from 'three';
+	import NavBall from '../NavBall/NavBall.svelte';
 	import { MathUtils } from 'three';
 	import { onCollectionCreated } from '$lib/common/utils';
 	import type { EkfQuat } from '@rgs/bindings';
+	import { onMount } from 'svelte';
 
-	let targetRotation = new Quaternion();
+	let ninetyDegVerticalRot = new Quaternion();
+	ninetyDegVerticalRot.setFromAxisAngle(new Vector3(1, 0, 0), Math.PI / 2);
+
+	let targetRotation = new Quaternion(0, 0, 0, 1);
 	let latestQuat: EkfQuat | undefined = undefined;
-	targetRotation.set(0, 0, 0, 1);
 
-	let eulerRepr = new Euler(0, 0, 0, 'XYZ');
+	$: upVector = new Vector3(0, 1, 0).applyQuaternion(targetRotation);
+
+	$: adjustedTargetRotation = targetRotation.clone().multiply(ninetyDegVerticalRot);
+
+	let eulerRepr = new Euler();
 	eulerRepr.setFromQuaternion(targetRotation);
 
-	onCollectionCreated('EkfQuat', (msg: EkfQuat) => {
-		const quat = msg.quaternion;
-		latestQuat = msg;
+	// onCollectionCreated('EkfQuat', (msg: EkfQuat) => {
+	onMount(() => {
+		const mockQuat = new Quaternion(0, 0, 0, 1);
+		// Rotate 45 degrees around the z axis to mock
+		const rot = new Quaternion();
+		rot.setFromAxisAngle(new Vector3(-0.3, 0, 0), Math.PI / 4);
+		mockQuat.multiply(rot);
 
+		const mockEkf: EkfQuat = {
+			// W, X, Y, Z
+			quaternion: [mockQuat.w, mockQuat.x, mockQuat.y, mockQuat.z],
+			euler_std_dev: [0, 0, 0],
+			status: 0,
+			time_stamp: 0
+		};
+
+		const quat = mockEkf.quaternion;
+		latestQuat = mockEkf;
 		targetRotation.set(quat[1], quat[2], quat[3], quat[0]);
+		targetRotation.normalize();
 		targetRotation = targetRotation;
-		eulerRepr.setFromQuaternion(targetRotation);
-		eulerRepr = eulerRepr;
+		// The IMU is placed flat on the rocket, so the up vector is the x axis
+		// https://support.sbg-systems.com/sc/qd/latest/reference-manual/conventions
+		const baseVec = new Vector3(0, -1, 0).applyQuaternion(targetRotation);
+		// eulerRepr is derived from upVector
+		eulerRepr = vectorToEuler(baseVec, 'XYZ');
 	});
+
+	// Is the up vector pointing above the horizon?
+	function upright(quaternion: Quaternion) {
+		const upDirection = new Vector3(0, 1, 0);
+		const localUpDirection = upDirection.clone().applyQuaternion(quaternion);
+		const dotProduct = localUpDirection.dot(upDirection);
+		return dotProduct > 0;
+	}
+
+	function adjustedPitch(quaternion: Quaternion) {
+		const upDirection = new Vector3(0, 1, 0);
+		const localUpDirection = upDirection.clone().applyQuaternion(quaternion);
+		const dotProduct = localUpDirection.dot(upDirection);
+		const angle = Math.acos(dotProduct);
+		return angle;
+	}
+
+	function vectorToEuler(direction: Vector3, eulerOrder: EulerOrder = 'XYZ'): Euler {
+		// Create a clone of the direction vector
+		const directionClone = direction.clone();
+
+		// Create a 'up' vector
+		const up = new Vector3(0, 1, 0);
+
+		// Create a 'right' vector
+		const right = new Vector3().crossVectors(up, directionClone).normalize();
+
+		// Recompute the 'up' vector
+		up.crossVectors(directionClone, right).normalize();
+
+		// Create a rotation matrix
+		const rotationMatrix = new Matrix4().makeBasis(right, up, directionClone);
+
+		// Create a Euler and set the order of rotation
+		const rotation = new Euler().setFromRotationMatrix(rotationMatrix, eulerOrder);
+
+		return rotation;
+	}
 </script>
 
 <div class="w-full h-full p-2 flex flex-col">
@@ -42,26 +105,45 @@
 		</div>
 
 		<div class="flex">
-			<span class="flex-1">Quat</span>
+			<span class="flex-1">Quat (W,X,Y,Z)</span>
+			<span class="flex-1">{targetRotation.w.toFixed(2)}</span>
 			<span class="flex-1">{targetRotation.x.toFixed(2)}</span>
 			<span class="flex-1">{targetRotation.y.toFixed(2)}</span>
 			<span class="flex-1">{targetRotation.z.toFixed(2)}</span>
-			<span class="flex-1">{targetRotation.w.toFixed(2)}</span>
 		</div>
 
 		<!-- Pitch yaw row -->
 		<div class="grid grid-cols-2">
 			<span>Roll</span>
-			<span class="text-right"
-				>{MathUtils.radToDeg(latestQuat?.euler_std_dev[0] ?? 0).toFixed(2)}°</span
-			>
+			<span class="text-right">{MathUtils.radToDeg(eulerRepr.x ?? 0).toFixed(2)}°</span>
 			<span>Pitch</span>
-			<span class="text-right"
-				>{MathUtils.radToDeg(latestQuat?.euler_std_dev[1] ?? 0).toFixed(2)}°</span
-			>
+			<span class="text-right">{MathUtils.radToDeg(eulerRepr.y ?? 0).toFixed(2)}°</span>
 			<span>Yaw</span>
-			<span class="text-right"
-				>{MathUtils.radToDeg(latestQuat?.euler_std_dev[2] ?? 0).toFixed(2)}°</span
+			<span class="text-right">{MathUtils.radToDeg(eulerRepr.z ?? 0).toFixed(2)}°</span>
+		</div>
+
+		<!-- Pointing direction -->
+		<div class="flex">
+			<span class="flex-1">Pointing</span>
+			<span class="flex-1">{upright(targetRotation) ? 'Up' : 'Down'}</span>
+		</div>
+
+		<!-- UP VECTOR -->
+		<div class="flex">
+			<span class="flex-1">Up Vector</span>
+			<span class="flex-1"
+				>{upVector
+					.toArray()
+					.map((v) => v.toFixed(2))
+					.join(', ')}</span
+			>
+		</div>
+
+		<!-- Up Angle -->
+		<div class="flex">
+			<span class="flex-1">Up Angle</span>
+			<span class="flex-1"
+				>{MathUtils.radToDeg(adjustedPitch(adjustedTargetRotation)).toFixed(2)}°</span
 			>
 		</div>
 	</div>
