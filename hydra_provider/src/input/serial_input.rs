32994bb2c4002b47fb5060fd5f17bdbb48848ca0
MarwanMashaly1 <mmash102@uottawa.ca>
2023-08-19 10:44:18 -0400

    send mavlink message

--- hydra_provider/src/input/serial_input.rs
+++ hydra_provider/src/input/serial_input.rs
@@ -4,16 +4,26 @@ use anyhow::Ok;
 use anyhow::Result;
 use log::{error, trace};
 
+use messages::command::Command;
+use messages::command::CommandData;
+use messages::command::DeployDrogue;
+use messages::command::DeployMain;
 use messages::mavlink;
 use messages::mavlink::uorocketry;
+use messages::mavlink::uorocketry::MavMessage;
+use messages::mavlink::uorocketry::POSTCARD_MESSAGE_DATA;
 use messages::mavlink::MavConnection;
+use messages::sender;
+use messages::Data;
 use messages::Message;
 use postcard::from_bytes;
+use postcard::to_vec;
 use serialport::available_ports;
 use std::sync::mpsc::Sender;
 
 use std::thread;
 use std::time::Duration;
+use std::time::SystemTime;
 
 pub struct SerialInput {
     link: Box<dyn MavConnection<uorocketry::MavMessage> + Send + Sync>,
@@ -57,10 +67,63 @@ impl SerialInput {
                 }
                 thread::sleep(Duration::from_secs(1));
             });
+
+            s.spawn(|| loop {
+                let mut input = String::new();
+                std::io::stdin()
+                    .read_line(&mut input)
+                    .expect("No string inputted");
+
+                let cmd: CommandData;
+
+                if input.contains("Fire dr") {
+                    cmd = CommandData::DeployDrogue(DeployDrogue { val: true });
+                    println!("Drogue deployed");
+                } else if input.contains("Fire main") {
+                    cmd = CommandData::DeployMain(DeployMain { val: true });
+                    println!("Main deployed");
+                } else {
+                    println!("Invalid command");
+                    continue;
+                }
+
+                let data = Data::Command(Command { data: cmd });
+                let time = fugit::Instant::<u64, 1, 1000>::from_ticks(
+                    SystemTime::now()
+                        .duration_since(SystemTime::UNIX_EPOCH)
+                        .unwrap()
+                        .as_millis() as u64,
+                );
+                let sender = sender::Sender::GroundStation;
+                let msg = Message::new(&time, sender, data);
+                if let Err(e) = self.send_message(msg) {
+                    error!("Error sending message: {:?}", e);
+                }
+            });
         });
 
         // To satisfy that we never return.
         panic!("Serial threads ended!");
+    }
+
+    fn send_message(&self, msg: Message) -> Result<()> {
+        let postcard_bytes = to_vec(&msg).expect("Failed to serialize message");
+        let postcard_data = POSTCARD_MESSAGE_DATA {
+            message: postcard_bytes,
+        };
+        let data = MavMessage::POSTCARD_MESSAGE(postcard_data);
+        println!("Sending message: {:?}", msg);
+
+        let header = mavlink::MavHeader {
+            system_id: 0,
+            component_id: 0,
+            sequence: 0,
+        };
+
+        // FIX: Message doesn't get sent it gets stuck here for some reason
+        self.link.send(&header, &data).unwrap();
+        println!("Sent message");
+        Ok(())
     }
 
     fn send_heartbeat(&self) -> Result<()> {
