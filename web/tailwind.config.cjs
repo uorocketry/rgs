2a0ec5196c77319cb1074ae61363736150189e7e
Fernando Nogueira <abert036@uottawa.ca>
2023-08-29 23:49:42 -0400

    improve ground station contrast

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,4 +1,21 @@
 module.exports = {
 	content: ['./src/**/*.{svelte,js,ts}'],
-	plugins: [require('@tailwindcss/typography'), require('daisyui')]
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				rgs: {
+					primary: '#006DB7',
+					secondary: '#EC1C24',
+					accent: '#FFCF00',
+					neutral: '#EFEFEF',
+					'base-100': '#FFFFFF',
+					info: '#FFCF00',
+					success: '#36D399',
+					warning: '#FFCF00',
+					error: '#F87272'
+				}
+			}
+		]
+	}
 };
