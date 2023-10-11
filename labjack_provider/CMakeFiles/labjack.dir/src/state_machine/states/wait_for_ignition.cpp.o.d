e789fa9c942684c902b943e138ec98e51bb53913
Noah Sprenger <noah.r.sprenger@gmail.com>
2023-10-11 12:48:32 -0400

    Cleanup local build files and update gitignore

--- labjack_provider/CMakeFiles/labjack.dir/src/state_machine/states/wait_for_ignition.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/labjack.dir/src/state_machine/states/wait_for_ignition.cpp.o: \
- /home/ns/Rocketry/rgs/labjack_provider/src/state_machine/states/wait_for_ignition.cpp \
- /usr/include/stdc-predef.h
