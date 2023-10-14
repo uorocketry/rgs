2d4c9fa6520eb6e70afa0ad3f805bcf11c2d8277
Fernando Nogueira <abert036@uottawa.ca>
2023-10-14 11:46:43 -0400

    feat: use cubemap instead of sky (#88)

--- web/src/lib/components/dumb/NavBall/NavBallScene.svelte
+++ web/src/lib/components/dumb/NavBall/NavBallScene.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Rocket from '$lib/components/models/Rocket.svelte';
 	import { T } from '@threlte/core';
-	import { HTML, OrbitControls, useTexture } from '@threlte/extras';
+	import { OrbitControls, useTexture } from '@threlte/extras';
 	import { tweened, type Tweened } from 'svelte/motion';
 	import { Euler, Quaternion, type EulerOrder } from 'three';
 	const map = useTexture('textures/navball.png');
@@ -43,7 +43,7 @@
 	fov={60}
 	position={[0, 35, 0]}
 	near={0.1}
-	far={100}
+	far={1000}
 	aspect={1}
 	on:create={({ ref }) => {
 		ref.lookAt(0, 1, 0);
@@ -51,10 +51,6 @@
 >
 	<OrbitControls enableDamping />
 </T.PerspectiveCamera>
-
-<T.AmbientLight intensity={1} />
-
-<HTML></HTML>
 
 <T.PolarGridHelper args={[15, 15, 8, 64]} />
 
@@ -66,7 +62,6 @@
 			<T.SphereGeometry args={[10, 64, 32]} />
 			<T.MeshStandardMaterial map={tex} />
 		</T.Mesh>
-		<!-- Another DOT sphere geometry with position y = 0.5 -->
 		<T.Mesh position.y="10">
 			<T.SphereGeometry args={[0.3, 10, 2]} />
 			<T.MeshStandardMaterial color={[1, 0, 1]} />
