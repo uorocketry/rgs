ce10f92b5e99bde5c0219c15b5ca184bbcaec046
Fernando Nogueira <fernando.nogueira@tradingcentral.com>
2023-10-19 19:01:35 -0400

    update: setup scripts

--- scripts/requirements/1_node.sh
+++ scripts/requirements/1_node.sh
@@ -1,23 +1,14 @@
 # bin/bash -i
 
+echo '📥 Installing NODE'
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install node
+nvm use node
 
-if [ -d "$~/.nvm" ];
-then
-    echo '✅ NODE is already installed'
-    node
-else 
-    echo '📥 Installing NODE'
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    nvm install node
-    nvm use node
+# Add zsh completions
+echo 'export NVM_DIR=~/.nvm' >> ~/.zshrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> ~/.zshrc
 
-    # Add zsh completions
-    echo 'export NVM_DIR=~/.nvm' >> ~/.zshrc
-    echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> ~/.zshrc
-
-    echo '✅ NODE installed'
-fi
+echo '✅ NODE installed'
--- scripts/requirements/3_pm2.sh
+++ scripts/requirements/3_pm2.sh
@@ -1,10 +1,10 @@
 # bin/bash -i
 
-if command -v pm2 &> /dev/null
-then
-    echo '✅ PM2 is already installed'
-else 
-    echo '📥 Installing PM2'
-    pnpm install -g pm2
-    echo '✅ PM2 installed'
-fi
+export SHELL=/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use node
+
+echo '📥 Installing PM2'
+npm install -g pm2
+echo '✅ PM2 installed'
