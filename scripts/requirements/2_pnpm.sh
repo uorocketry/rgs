bc80c03ee410d32506132f3cd1aa12a8e21e630b
Fernando Nogueira <fernando.nogueira@tradingcentral.com>
2023-10-19 19:01:35 -0400

    update: setup scripts

--- scripts/requirements/2_pnpm.sh
+++ scripts/requirements/2_pnpm.sh
@@ -1,28 +1,10 @@
 # bin/bash -i
 
 export SHELL=/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use node
 
-# Check if pnpm is installed
-if [ -d ~/.local/share/pnpm ]
-then
-    echo '✅ PNPM is already installed'
-else 
-    echo '📥 Installing PNPM'
-    curl -fsSL https://get.pnpm.io/install.sh | bash -
-    PNPM_HOME="$HOME/.local/share/pnpm"
-    case ":$PATH:" in
-    *":$PNPM_HOME:"*) ;;
-    *) export PATH="$PNPM_HOME:$PATH" ;;
-    esac
-    pnpm setup
-    
-    // Install ZSH completions
-    echo '# PNPM configuration' >> ~/.zshrc
-    echo 'PNPM_HOME="$HOME/.local/share/pnpm"' >> ~/.zshrc
-    echo 'case ":$PATH:" in' >> ~/.zshrc
-    echo '*":$PNPM_HOME:"*) ;;' >> ~/.zshrc
-    echo '*) export PATH="$PNPM_HOME:$PATH" ;;' >> ~/.zshrc
-    echo 'esac' >> ~/.zshrc
-
-    echo '✅ PNPM installed'
-fi
+echo '📥 Installing PNPM'
+npm install -g pnpm
+pnpm setup
