fc9fb392857e3db4434035ae17b133ea8cb7a482
Fernando Nogueira <abert036@uottawa.ca>
2023-10-21 12:39:43 -0400

    Major refactor: replace daisyUI by the Skeleton UI toolkit (#91)

---
+++ web/tailwind.config.ts
@@ -0,0 +1,21 @@
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { join } from 'path';
+import { uorocketryTheme } from './src/lib/common/uorocketryTheme';
+
+const config = {
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{svelte,js,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
+	plugins: [
+		require('@tailwindcss/typography'),
+		skeleton({
+			themes: {
+				custom: [uorocketryTheme]
+			}
+		})
+	]
+};
+
+export default config;
