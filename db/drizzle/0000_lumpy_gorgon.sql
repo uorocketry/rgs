9d2aa270b26a4e110eb2a66e8d5fdc5e11bb630e
Fernando Nogueira <abert036@uottawa.ca>
2023-10-22 23:26:28 -0400

    chore: remove experiment

--- db/drizzle/0000_lumpy_gorgon.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `users` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`full_name` text(256) DEFAULT '' NOT NULL,
-	`last_name` text(256) DEFAULT '' NOT NULL
-);
---> statement-breakpoint
-CREATE INDEX `name_idx` ON `users` (`full_name`);
