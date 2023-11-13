69c4e2e23ffae59c9f66c92f6112b68364208702
Fernando Nogueira <abert036@uottawa.ca>
2023-11-12 23:57:22 -0500

    wip:

--- labjack_provider/src/main.cpp
+++ labjack_provider/src/main.cpp
@@ -1,6 +1,39 @@
 #include "main.hpp"
 
-int main() {
+std::string exec(const char *cmd)
+{
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe)
+    {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+    {
+        // Buffer data has a json string
+        std::string buf = buffer.data();
+
+        // TODO: Convert this to a c++ object(?)
+    }
+    return result;
+}
+
+int main()
+{
+    try
+    {
+        std::string output = exec("../../../dbmim/main --record Air"); // Replace with your program's path
+        std::cout << "Output of the program:\n"
+                  << output << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+
     float load;
     double temp;
     int angle;
@@ -10,13 +43,14 @@ int main() {
     LabJack labjack = LabJack();
     servo.setup_servo(labjack);
     servo.write_angle(labjack, 0.0);
-	// MillisecondSleep(5000);
+    // MillisecondSleep(5000);
     servo.write_angle(labjack, 180.0);
     // Set up a flag to control the loop
     std::atomic<bool> exitFlag = false;
 
     // Create a thread for non-blocking input
-    std::thread inputThread([&exitFlag]() {
+    std::thread inputThread([&exitFlag]()
+                            {
         while (true) {
             std::string userInput;
             std::cout << "Enter 'q' to quit: ";
@@ -37,11 +71,11 @@ int main() {
 
             // Print user input
             std::cout << "You entered: " << userInput << std::endl;
-        }
-    });
+        } });
     double i = 0.0;
     // Main loop
-    while (!exitFlag) {
+    while (!exitFlag)
+    {
         // Your main loop logic here
         // temp = thermocouple.read_temperature(handle);
         // load = load_cell.read_weight(handle);
@@ -52,13 +86,10 @@ int main() {
         // servo.write_angle(handle, i);
         // std::this_thread::sleep_for(std::chrono::milliseconds(100));
         // i++;
-
     }
 
     // Wait for the input thread to finish
     inputThread.join();
-
-
 
     std::cout << "Exiting the program." << std::endl;
     return 0;
