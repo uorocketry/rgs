894d631eacc7322e3664aa73b19e469d34c5feda
Fernando Nogueira <abert036@uottawa.ca>
2024-01-20 12:07:33 -0500

    chore: add health check

--- hydra_provider/bin/server.rs
+++ hydra_provider/bin/server.rs
@@ -1,7 +1,10 @@
+use std::time::Duration;
+
 use tonic::{transport::Server, Request, Response, Status};
 
 use hello_world::greeter_server::{Greeter, GreeterServer};
 use hello_world::{HelloReply, HelloRequest};
+use tonic_health::server::HealthReporter;
 
 pub mod hello_world {
     tonic::include_proto!("helloworld");
@@ -25,14 +28,38 @@ impl Greeter for MyGreeter {
     }
 }
 
+/// This function (somewhat improbably) flips the status of a service every second, in order
+/// that the effect of `tonic_health::HealthReporter::watch` can be easily observed.
+async fn twiddle_service_status(mut reporter: HealthReporter) {
+    let mut iter = 0u64;
+    loop {
+        iter += 1;
+        tokio::time::sleep(Duration::from_secs(1)).await;
+
+        if iter % 2 == 0 {
+            reporter.set_serving::<GreeterServer<MyGreeter>>().await;
+        } else {
+            reporter.set_not_serving::<GreeterServer<MyGreeter>>().await;
+        };
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
+    health_reporter
+        .set_serving::<GreeterServer<MyGreeter>>()
+        .await;
+
+    tokio::spawn(twiddle_service_status(health_reporter.clone()));
+
     let addr = "[::1]:50051".parse().unwrap();
     let greeter = MyGreeter::default();
 
-    println!("GreeterServer listening on {}", addr);
+    println!("HealthServer + GreeterServer listening on {}", addr);
 
     Server::builder()
+        .add_service(health_service)
         .add_service(GreeterServer::new(greeter))
         .serve(addr)
         .await?;
