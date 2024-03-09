d9a8bb15c9dd97cd92e7fb1098c4c604be00f49d
AnjelikaBab <banjelika6@gmail.com>
2024-03-09 13:52:40 -0500

    altitude

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,5 +1,12 @@
+<script lang="ts">
+	import { LatestAltitudeMeasurements } from "./types"
+</script>
+
 <div>
 	<article class="article">
 		<h1>RGS Ground server</h1>
+
+		
+		My altitude is {$LatestAltitudeMeasurements.data?.rocket_sensor_air[0]?.altitude}
 	</article>
 </div>
