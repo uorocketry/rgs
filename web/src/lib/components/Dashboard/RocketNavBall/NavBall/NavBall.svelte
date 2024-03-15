1644a2ff03f228f6c2b41abeaed321a4699dc519
Fernando Nogueira <abert036@uottawa.ca>
2024-03-15 17:57:28 -0400

    Navball heading indicator (#115)

--- web/src/lib/components/Dashboard/RocketNavBall/NavBall/NavBall.svelte
+++ web/src/lib/components/Dashboard/RocketNavBall/NavBall/NavBall.svelte
@@ -4,11 +4,38 @@
 	import { Quaternion } from 'three/src/Three.js';
 	import NavBallScene from './NavBallScene.svelte';
 	export let targetRotation: Quaternion = new Quaternion();
+
+	let w = 500;
+	let h = 500;
+	$: squareSize = Math.min(w - 80, h - 160);
 </script>
 
-<div class="flex-1 w-full h-full">
-	<Canvas>
-		<Environment path={'/textures/cubemap/'} files={'road.hdr'} isBackground={true} />
-		<NavBallScene bind:targetRotation />
-	</Canvas>
+<div bind:clientHeight={h} bind:clientWidth={w} class="grid place-items-center w-full h-full">
+	<div
+		class="navball-wrapper relative"
+		style="
+	width: {squareSize}px;
+	height: {squareSize}px;
+"
+	>
+		<Canvas
+			size={{
+				height: 500,
+				width: 500
+			}}
+		>
+			<Environment path={'/textures/cubemap/'} files={'road.hdr'} isBackground={true} />
+
+			<NavBallScene bind:targetRotation />
+		</Canvas>
+	</div>
 </div>
+
+<style>
+	:global(.navball-wrapper canvas) {
+		min-width: 100%;
+		min-height: 100%;
+		max-width: 100%;
+		max-height: 100%;
+	}
+</style>
