e943715fdd985d75305ad379e43c475a62549c1e
Fernando Nogueira <abert036@uottawa.ca>
2024-04-06 08:19:36 -0400

    nit:

--- web/src/lib/components/Dashboard/RocketStatus/RocketStatus.svelte
+++ web/src/lib/components/Dashboard/RocketStatus/RocketStatus.svelte
@@ -11,36 +11,6 @@
 			</tr>
 		</thead>
 		<tbody>
-			<!-- <tr class="hover clicky cursor-pointer">
-				<div class="tooltip tooltip-right" data-tip="Radio Connection status with hydra">
-					<td>
-						<span class="text-left">Radio Connection</span>
-					</td>
-				</div>
-				<td>
-					<span class="text-right">{$linkStatus?.connected ? 'Connected' : 'Disconnected'}</span>
-				</td>
-			</tr> -->
-			<!-- <tr class="hover clicky cursor-pointer">
-				<div class="tooltip tooltip-right" data-tip="Current state of hydra">
-					<td>
-						<span class="text-left font-bold">State</span>
-					</td>
-				</div>
-				<td>
-					<span class="text-right">{$state?.state ?? 'Unknown'}</span>
-				</td>
-			</tr> -->
-			<!-- <tr class="hover clicky cursor-pointer">
-				<div class="tooltip tooltip-right" data-tip="Number of missed messages from hydra">
-					<td>
-						<span class="text-left">Missed Messages</span>
-					</td>
-				</div>
-				<td>
-					<span class="text-right">{$linkStatus?.missed_messages ?? '?'}</span>
-				</td>
-			</tr> -->
 			<tr class="hover clicky cursor-pointer">
 				<div class="tooltip tooltip-right" data-tip="Current Pressure of rocket">
 					<td>
@@ -109,95 +79,6 @@
 					</span></td
 				>
 			</tr>
-			<!-- <tr class="hover clicky cursor-pointer">
-				<div
-					class="tooltip tooltip-right"
-					data-tip="Altitude from where we are from above sea level"
-				>
-					<td>
-						<span class="text-left font-bold">Ground Altitude</span>
-					</td>
-				</div>
-				<td>
-					<span class="text-right">{$ground_altitude}</span>
-				</td>
-			</tr> -->
-			<!-- <tr class="hover clicky cursor-pointer">
-				<div class="tooltip tooltip-right" data-tip="The maximium altitude reached by the rocket">
-					<td>
-						<span class="text-left font-bold">Max Altitude</span>
-					</td>
-				</div>
-				<td>
-					<span class="text-right">{$max_altitude}</span>
-				</td>
-			</tr> -->
-			<!-- <tr class="hover clicky cursor-pointer">
-				<div class="tooltip tooltip-right" data-tip="Defined Target Altitude">
-					<td>
-						<span class="text-left">Target Altitude</span>
-					</td>
-				</div>
-				<td>
-					<span class="text-right">{$flightDirector?.targetAltitude ?? '?'}</span>
-				</td>
-			</tr> -->
-			<!-- <tr class="hover clicky cursor-pointer">
-				<div
-					class="tooltip tooltip-right"
-					data-tip="Defined Relative Altitude. How much we are above so level"
-				>
-					<td>
-						<span class="text-left">Relative Altitude</span>
-					</td>
-				</div>
-				<td>
-					<span class="text-right">{$flightDirector?.relativeAltitude ?? '?'}</span>
-				</td>
-			</tr> -->
-			<!-- <tr class="hover clicky cursor-pointer">
-				<div class="tooltip tooltip-right" data-tip="How far are we from the taraget altitude set">
-					<td>
-						<span class="text-left font-bold">Distance from Target Altitude</span>
-					</td>
-				</div>
-				<td>
-					<span class="text-right">{$distance_from_target}</span>
-				</td>
-			</tr> -->
-			<!-- <tr class="hover clicky cursor-pointer">
-				<div
-					class="tooltip tooltip-right"
-					data-tip="How far have we traveled from the launch point"
-				>
-					<td>
-						<span class="text-left">Total Traveled Distance</span>
-					</td>
-				</div>
-				<td>
-					<span class="text-right">{$total_traveled_distance}</span>
-				</td>
-			</tr> -->
-			<!-- <tr class="hover clicky cursor-pointer">
-				<div class="tooltip tooltip-right" data-tip="G force experienced by the rocket">
-					<td>
-						<span class="text-left">G Force</span>
-					</td>
-				</div>
-				<td>
-					<span class="text-right">{$g_force}</span>
-				</td>
-			</tr> -->
-			<!-- <tr class="hover clicky cursor-pointer">
-				<div class="tooltip tooltip-right" data-tip="Max G force experienced by the rocket">
-					<td>
-						<span class="text-left">Max G Force</span>
-					</td>
-				</div>
-				<td>
-					<span class="text-right">{$max_g_force}</span>
-				</td>
-			</tr> -->
 		</tbody>
 	</table>
 </div>
