86ae5f2abfc8fc88b9a8409653e9499f8bfd8dc6
Fernando Nogueira <abert036@uottawa.ca>
2024-08-17 14:18:28 -0400

    wip:

--- hydra_provider/src/database_service/hydra_input/messages/sensor/gps_velocity.rs
+++ hydra_provider/src/database_service/hydra_input/messages/sensor/gps_velocity.rs
@@ -2,27 +2,44 @@ use crate::database_service::hydra_input::saveable::SaveableData;
 use messages::sensor::GpsVel;
 use sqlx::{postgres::PgQueryResult, query, Error, Postgres, Transaction};
 
+fn option_numeric_to_i32<T: Into<i32>>(value: Option<T>) -> Option<i32> {
+    match value {
+        Some(value) => Some(value.into()),
+        None => None,
+    }
+}
+
 impl SaveableData for GpsVel {
     async fn save(
         &self,
         transaction: &mut Transaction<'_, Postgres>,
         rocket_message_id: i32,
     ) -> Result<PgQueryResult, Error> {
+        let velocity = match self.velocity {
+            Some(arr) => arr.iter().map(|&x| Some(x as f32)).collect(),
+            None => vec![None, None, None],
+        };
+        let status_as_i32 = self
+            .status
+            .get_status()
+            .map(|status| status as i32)
+            .unwrap_or(-1);
+        let time_of_week = match self.time_of_week {
+            Some(time_of_week) => Some(time_of_week as i32),
+            None => None,
+        };
+
         query!(
-			"INSERT INTO public.rocket_sensor_gps_vel (rocket_sensor_message_id,time_stamp,status,time_of_week,velocity_x,velocity_y,velocity_z,velocity_acc_x,velocity_acc_y,velocity_acc_z,course,course_acc)
-			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)",
+			"INSERT INTO public.rocket_sensor_gps_vel (rocket_sensor_message_id, time_stamp, status, time_of_week, velocity_x, velocity_y, velocity_z, course)
+			VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
 			rocket_message_id,
 			self.time_stamp as i32,
-			self.status as i32,
-			self.time_of_week as i32,
-			self.velocity[0],
-			self.velocity[1],
-			self.velocity[2],
-			self.velocity_acc[0],
-			self.velocity_acc[1],
-			self.velocity_acc[2],
-			self.course as f32,
-			self.course_acc as f32,
+			status_as_i32,
+			time_of_week,
+			velocity[0],
+			velocity[1],
+			velocity[2],
+			self.course,
 		)
 		.execute(&mut **transaction)
 		.await
--- hydra_provider/src/database_service/hydra_input/messages/sensor/imu1.rs
+++ hydra_provider/src/database_service/hydra_input/messages/sensor/imu1.rs
@@ -2,39 +2,39 @@ use crate::database_service::hydra_input::saveable::SaveableData;
 use messages::sensor::Imu1;
 use sqlx::{postgres::PgQueryResult, query, Error, Postgres, Transaction};
 
-// export const rocket_sensor_imu_1 = pgTable("rocket_sensor_imu_1", {
-//     rocket_sensor_message_id: integer("rocket_sensor_message_id")
-//         .references(() => rocket_sensor_message.rocket_message_id)
-//         .notNull()
-//         .primaryKey(),
-//     time_stamp: integer("time_stamp").notNull(),
-//     status: integer("status").notNull(),
-//     accelorometer_x: real("accelorometer_x"),
-//     accelorometer_y: real("accelorometer_y"),
-//     accelorometer_z: real("accelorometer_z"),
-//     gyroscope_x: real("gyroscope_x"),
-//     gyroscope_y: real("gyroscope_y"),
-//     gyroscope_z: real("gyroscope_z"),
-// });
-
 impl SaveableData for Imu1 {
     async fn save(
         &self,
         transaction: &mut Transaction<'_, Postgres>,
         rocket_message_id: i32,
     ) -> Result<PgQueryResult, Error> {
+        let accelerometers = match self.accelerometers {
+            Some(arr) => arr.iter().map(|&x| Some(x as f32)).collect(),
+            None => vec![None, None, None],
+        };
+        let status_as_i32 = self
+            .status
+            .get_flags()
+            .map(|status| status.bits() as i32)
+            .unwrap_or(-1);
+
+        let gyroscopes = match self.gyroscopes {
+            Some(arr) => arr.iter().map(|&x| Some(x as f32)).collect(),
+            None => vec![None, None, None],
+        };
+
         query!(
             "INSERT INTO public.rocket_sensor_imu_1 (rocket_sensor_message_id,time_stamp,status,accelorometer_x,accelorometer_y,accelorometer_z,gyroscope_x,gyroscope_y,gyroscope_z)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
             rocket_message_id,
             self.time_stamp as i32,
-            self.status as i32,
-            self.accelerometers[0],
-            self.accelerometers[1],
-            self.accelerometers[2],
-            self.gyroscopes[0],
-            self.gyroscopes[1],
-            self.gyroscopes[2],
+            status_as_i32,
+            accelerometers[0],
+            accelerometers[1],
+            accelerometers[2],
+            gyroscopes[0],
+            gyroscopes[1],
+            gyroscopes[2],
         )
         .execute(&mut **transaction)
         .await
