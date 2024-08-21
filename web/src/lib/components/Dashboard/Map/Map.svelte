90210e5948e7373358398c8f81c5c446d6233c8a
MarwanMashaly1 <mmash102@uottawa.ca>
2024-08-21 19:24:28 -0400

    Add sbg position

--- web/src/lib/components/Dashboard/Map/Map.svelte
+++ web/src/lib/components/Dashboard/Map/Map.svelte
@@ -3,7 +3,7 @@
 	import { Viewer } from 'cesium';
 	import 'cesium/Build/Cesium/Widgets/widgets.css';
 	import * as Cesium from 'cesium';
-	import { LatestAltitude, LatestCoordinates } from './types';
+	import { LatestAltitude, LatestCoordinates, LatestCoordinatesSBG } from './types';
 
 	// TODOS:
 	// - Add launch coordinates
@@ -12,49 +12,68 @@
 	// @ts-expect-error Custom window property
 	window.CESIUM_BASE_URL = 'node_modules/cesium/Build/Cesium';
 
-	console.log('Cesium', Cesium);
-	let viewer: Viewer;
+	let viewer: Viewer | undefined;
 
 	let rocketModel: Cesium.Entity | undefined;
-
-	let latestCoordinates = {
-		latitude: 0,
-		longitude: 0,
-		altitude: 0
-	};
+	let rocketModelSBG: Cesium.Entity | undefined;
 
 	$: latestCoordinatesData = $LatestCoordinates.data;
+	$: LatestCoordinatesSBGData = $LatestCoordinatesSBG.data;
 	$: LatestAltitudeData = $LatestAltitude.data;
 	console.log('Latest coordinates', latestCoordinatesData);
 	console.log('Latest altitude', LatestAltitudeData);
 
-	$: {
-		latestCoordinates = {
-			latitude: latestCoordinatesData?.rocket_sensor_nav_pos_llh[0]?.latitude ?? 0,
-			longitude: latestCoordinatesData?.rocket_sensor_nav_pos_llh[0]?.longitude ?? 0,
-			altitude: LatestAltitudeData?.rocket_sensor_air[0]?.altitude ?? 0
-		};
+	$: latestCoordinates = {
+		latitude: latestCoordinatesData?.rocket_sensor_nav_pos_llh[0]?.latitude ?? 0,
+		longitude: latestCoordinatesData?.rocket_sensor_nav_pos_llh[0]?.longitude ?? 0,
+		altitude: LatestAltitudeData?.rocket_sensor_air[0]?.altitude ?? 0
+	};
 
-		if (viewer && rocketModel) {
-			console.log('Updating label text and position');
+	$: latestCoordinatesSBG = {
+		latitude: LatestCoordinatesSBGData?.rocket_sensor_gps_pos_1[0]?.latitude ?? 0,
+		longitude: LatestCoordinatesSBGData?.rocket_sensor_gps_pos_1[0]?.longitude ?? 0
+	};
 
-			// Update label text
-			if (rocketModel.label) {
-				rocketModel.label.text = new Cesium.ConstantProperty(
-					`Latitude: ${latestCoordinates.latitude}, Longitude: ${latestCoordinates.longitude}, Altitude: ${latestCoordinates.altitude} meters`
-				);
-			}
+	$: if (viewer && rocketModel) {
+		console.log('Updating label text and position');
 
-			// Calculate position
-			const zPosition = latestCoordinates.altitude; // adjust scale factor etc
-			rocketModel.position = new Cesium.ConstantPositionProperty(
-				Cesium.Cartesian3.fromDegrees(
-					latestCoordinates.longitude,
-					latestCoordinates.latitude,
-					zPosition
-				)
+		// Update label text
+		if (rocketModel.label) {
+			rocketModel.label.text = new Cesium.ConstantProperty(
+				`Backup GPS Latitude: ${latestCoordinates.latitude}, Longitude: ${latestCoordinates.longitude}, Altitude: ${latestCoordinates.altitude - 175} meters`
 			);
 		}
+
+		// Calculate position
+		const zPosition = latestCoordinates.altitude; // adjust scale factor etc
+		rocketModel.position = new Cesium.ConstantPositionProperty(
+			Cesium.Cartesian3.fromDegrees(
+				latestCoordinates.longitude as number, //TODO get type
+				latestCoordinates.latitude as number, //TODO get type
+				zPosition
+			)
+		);
+	}
+
+	$: if (viewer && rocketModelSBG) {
+		console.log('Updating label text and position');
+
+		// Update label text
+		if (rocketModelSBG.label) {
+			rocketModelSBG.label.text = new Cesium.ConstantProperty(
+				`SBG GPS Latitude: ${latestCoordinatesSBG.latitude}, Longitude: ${latestCoordinatesSBG.longitude}, Altitude: ${latestCoordinatesSBG.altitude - 175} meters`
+			);
+		}
+
+		// Calculate position
+		const zPosition = latestCoordinates.altitude + 10; // adjust scale factor etc
+		rocketModelSBG.position = new Cesium.ConstantPositionProperty(
+			Cesium.Cartesian3.fromDegrees(
+				latestCoordinatesSBG.longitude as number, //TODO get type
+				latestCoordinatesSBG.latitude as number, //TODO get type
+				zPosition
+			)
+		);
 	}
 
 	onMount(async () => {
@@ -96,7 +115,7 @@
 			position: launchpadPositon,
 			point: {
 				pixelSize: 10,
-				color: Cesium.Color.BLUE
+				color: Cesium.Color.GREEN
 			},
 			label: {
 				text: 'Launchpad',
@@ -112,19 +131,38 @@
 				runAnimations: false,
 				scale: 1.0, // Optional: Adjust the scale of the model if needed
 				minimumPixelSize: 50 // Optional: Minimum pixel size for the model*/
-			},
-			label: {
-				text: 'Coordinates: unknown', // Initial coordinates label
-				font: '14px sans-serif',
-				fillColor: Cesium.Color.YELLOW,
-				outlineColor: Cesium.Color.BLACK,
-				outlineWidth: 2,
-				style: Cesium.LabelStyle.FILL_AND_OUTLINE,
-				pixelOffset: new Cesium.Cartesian2(0, -100), // Offset label position
-				backgroundColor: Cesium.Color.WHITE
 			}
+			// label: {
+			// 	text: 'Coordinates: unknown', // Initial coordinates label
+			// 	font: '14px sans-serif',
+			// 	fillColor: Cesium.Color.YELLOW,
+			// 	outlineColor: Cesium.Color.BLACK,
+			// 	outlineWidth: 2,
+			// 	style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+			// 	pixelOffset: new Cesium.Cartesian2(0, -100), // Offset label position
+			// 	backgroundColor: Cesium.Color.WHITE
+			// }
 		});
 
+		rocketModelSBG = viewer.entities.add({
+			position: launchpadPositon,
+			model: {
+				uri: '/models/rocket.glb',
+				runAnimations: false,
+				scale: 1.0, // Optional: Adjust the scale of the model if needed
+				minimumPixelSize: 50 // Optional: Minimum pixel size for the model*/
+			}
+			// label: {
+			// 	text: 'Coordinates: unknown', // Initial coordinates label
+			// 	font: '14px sans-serif',
+			// 	fillColor: Cesium.Color.YELLOW,
+			// 	outlineColor: Cesium.Color.BLACK,
+			// 	outlineWidth: 2,
+			// 	style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+			// 	pixelOffset: new Cesium.Cartesian2(0, -100), // Offset label position
+			// 	backgroundColor: Cesium.Color.WHITE
+			// }
+		});
 		viewer.flyTo(rocketModel);
 		//viewer.flyTo(JMTS);
 
--- web/src/lib/components/Dashboard/Map/types.ts
+++ web/src/lib/components/Dashboard/Map/types.ts
@@ -13,6 +13,16 @@ const LatestCoordinatesDocument = graphql(`
 	}
 `);
 
+const LatestCoordinatesSBGDocument = graphql(`
+	subscription LatestCoordinates {
+		rocket_sensor_gps_pos_1(limit: 1, order_by: { rocket_sensor_message: { rocket_message: { created_at: desc } } }
+		) {
+			latitude
+			longitude
+		}
+	}
+`);
+
 const latestAltitudeDocument = graphql(`
 	subscription LatestAltitude {
 		rocket_sensor_air(limit: 1, order_by: { rocket_sensor_message: { rocket_message: { created_at: desc } } }
@@ -25,6 +35,11 @@ const latestAltitudeDocument = graphql(`
 export const LatestCoordinates = subscriptionStore({
 	client: gqlClient,
 	query: LatestCoordinatesDocument
+});
+
+export const LatestCoordinatesSBG = subscriptionStore({
+	client: gqlClient,
+	query: LatestCoordinatesSBGDocument
 });
 
 export const LatestAltitude = subscriptionStore({
