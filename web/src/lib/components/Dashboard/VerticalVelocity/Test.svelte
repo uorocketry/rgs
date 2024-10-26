fd7cc68cab6ec7967d3bd597c434b50dba8bca11
ccm32004 <chingching.ma32004@gmail.com>
2024-10-25 20:37:13 -0400

    removed unecessary files

---
+++ web/src/lib/components/Dashboard/VerticalVelocity/Test.svelte
@@ -0,0 +1,24 @@
+<script>
+    import BarChart from './Arrow/Bar.svelte';
+    import { onDestroy } from 'svelte';
+  
+    // Initial velocity value
+    let velocity = 0;
+  
+    // Function to simulate velocity updates (randomly change velocity between -50 and +50)
+    function updateVelocity() {
+      velocity = Math.floor(Math.random() * 100) - 50;
+    }
+  
+    // Update velocity every 2 seconds
+    const interval = setInterval(updateVelocity, 2000);
+  
+    // Clear interval when component is destroyed
+    onDestroy(() => clearInterval(interval));
+  </script>
+  
+  <main>
+    <h1>Dynamic Single-Bar Velocity Chart</h1>
+    <BarChart {velocity} />
+  </main>
+  
